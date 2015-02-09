f5bd7ebeb6a7cc27699775c125fd8f7fa11a604d
Henk Burgstra <henk.burgstra@gmail.com>
2015-02-09 16:37:05 +0100

    Tafels 11 en 12 ook meegenomen.

--- tafels.js
+++ tafels.js
@@ -9,7 +9,7 @@ tafelsApp.controller('TafelsCtrl', function ($scope, $timeout) {
 	$scope.magAntwoorden = false;
 	$scope.bedenktijd = 4;
 	$scope.counterTimeout = null;
-	$scope.tafels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	$scope.tafels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 	$scope.gekozenTafels = [];
 	$scope.mogelijkeAntwoorden = []; 
 	
